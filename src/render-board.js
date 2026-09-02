f05847cd6e2e4fa23044eda972125638741a3226
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-09-01 21:07:48 -0400

    Add rough draft of what the HTML will look like

--- src/render-board.js
+++ src/render-board.js
@@ -1,6 +1,12 @@
 //BOTH INITIAL DOM LOADING AND TURN-GENERATED DOM UPDATES
 
 export function loadDOM(){
+
+    
+
+    for(let i = 0; i < 10; i++){
+        //do stuff
+    }
     //initial DOM creation of needed elements for page?
     //generate the 10x10 board and attach data-attributes to each tile
 }
